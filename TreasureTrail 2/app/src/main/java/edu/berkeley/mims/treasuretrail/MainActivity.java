58f6106abb315563f7fc52c5b3bab6e1f82c004f
ruchita20 <ruchita@berkeley.edu>
2015-04-23 23:36:23 -0700

    data sync message update

--- TreasureTrail 2/app/src/main/java/edu/berkeley/mims/treasuretrail/MainActivity.java
+++ TreasureTrail 2/app/src/main/java/edu/berkeley/mims/treasuretrail/MainActivity.java
@@ -175,7 +175,7 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
     //Added by Dheera for phone's accelerometer part and datalogging from pebble
     private void updateUi() {
         System.out.print("Inside updateUi()");
-        Toast.makeText(MainActivity.this, "Syncing data from your Pebble watch.", Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "TreasureTrail - Data sync in progress.", Toast.LENGTH_LONG).show();
 
         TextView textView = (TextView) findViewById(R.id.log_data_text_view);
         //textView.setTextColor(Color.GREEN);
