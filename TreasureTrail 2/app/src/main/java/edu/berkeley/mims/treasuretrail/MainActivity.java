bdfff1ef7d44cb919fccc501e724306839b337ce
ruchita20 <ruchita@berkeley.edu>
2015-04-23 23:28:22 -0700

    goals

--- TreasureTrail 2/app/src/main/java/edu/berkeley/mims/treasuretrail/MainActivity.java
+++ TreasureTrail 2/app/src/main/java/edu/berkeley/mims/treasuretrail/MainActivity.java
@@ -522,7 +522,7 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             // View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            View rootView = inflater.inflate(R.layout.profilelayout, container, false);
+            View rootView = inflater.inflate(R.layout.activityfragment, container, false);
             return rootView;
         }
 
